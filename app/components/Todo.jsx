613cfdc0b4dfe45a5729bd2791c15f0b8417e318
Apostata <rene.souza@gmail.com>
2017-04-10 23:08:12 -0300

    Componentes TodoList e Todo adicionados

---
+++ app/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Todo extends React.Component{
+
+	render(){
+		let {text, id} = this.props;
+		return(
+			<div>
+				{id} - {text}
+			</div>
+		)
+	}
+}
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import TodoList from 'TodoList';
 
 export default class TodoApp extends React.Component{
+
+	constructor(){
+		super();
+		this.state ={
+			todos: [
+				{
+					id:1,
+					text: 'Passear com o cachorro!'
+				},
+				{
+					id: 2,
+					text: 'Limpar o jardim!'
+				},
+				{
+					id:3,
+					text: 'Estudar React'
+				},
+				{
+					id:4,
+					text: 'Fumar um cigarrinho'
+				}
+			]
+		}
+	}
+
 	render(){
+		let {todos} = this.state;
 		return(
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos}/>
 			</div>
 		)
 	}
